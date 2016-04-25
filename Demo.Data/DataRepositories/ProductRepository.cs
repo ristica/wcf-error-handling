4adcb9059af3f2785526c920a8ca115bc62a546c
aleks <ristica@users.noreply.github.com>
2016-04-25 12:37:12 +0200

    Update ProductRepository.cs

--- Demo.Data/DataRepositories/ProductRepository.cs
+++ Demo.Data/DataRepositories/ProductRepository.cs
@@ -1,50 +1,93 @@
-﻿using System.ComponentModel.Composition;
+using System.ComponentModel.Composition;
 using System.Linq;
 using Demo.Business.Entities;
 using Demo.Data.Contracts;
-using Demo.Data.Db;
+using System.Collections.Generic;
 
 namespace Demo.Data.DataRepositories
 {
     [Export(typeof(IProductRepository))]
-    [PartCreationPolicy(CreationPolicy.NonShared)]
     public class ProductRepository : IProductRepository
     {
+        #region Db
+
+        public List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+                ProductId = 1,
+                ArticleNumber = "syrgsr",
+                Description = "Test product 1",
+                IsActive = true,
+                Name = "Product 1",
+                Price = 100
+            },
+            new Product
+            {
+                ProductId = 2,
+                ArticleNumber = "vhjukjuh",
+                Description = "Test product 2",
+                IsActive = true,
+                Name = "Product 2",
+                Price = 200
+            },
+            new Product
+            {
+                ProductId = 3,
+                ArticleNumber = "23545",
+                Description = "Test product 3",
+                IsActive = true,
+                Name = "Product 3",
+                Price = 300
+            },
+            new Product
+            {
+                ProductId = 4,
+                ArticleNumber = "25545",
+                Description = "Test product 4",
+                IsActive = true,
+                Name = "Product 4",
+                Price = 400
+            }
+        };
+
+        #endregion
+
         #region IProductRepository implementation
 
         public Product GetProductByArticleNumber(string articleNumber)
         {
-            return ProductDb.Get().FirstOrDefault(p => p.ArticleNumber.Equals(articleNumber));
+            return Products.FirstOrDefault(p => p.ArticleNumber.Equals(articleNumber));
         }
 
         public Product GetProductById(int productId)
         {
-            return ProductDb.Get().FirstOrDefault(p => p.ProductId == productId);
+            return Products.FirstOrDefault(p => p.ProductId == productId);
         }
 
         public Product[] GetActiveProducts()
         {
-            return ProductDb.Get().Where(p => p.IsActive).ToArray();
+            return Products.Where(p => p.IsActive).ToArray();
         }
 
         public Product[] GetProducts()
         {
-            return ProductDb.Get().ToArray();
+            return Products.ToArray();
         }
 
         public void ActivateProduct(int productId)
         {
-            ProductDb.Get().FirstOrDefault(p => p.IsActive).IsActive = true;
+            Products.FirstOrDefault(p => p.IsActive).IsActive = true;
         }
 
         public void DeactivateProduct(int productId)
         {
-            ProductDb.Get().FirstOrDefault(p => p.IsActive).IsActive = false;
+            Products.FirstOrDefault(p => p.IsActive).IsActive = false;
         }
 
         public Product UpdateProduct(Product product)
         {
-            var p = ProductDb.Get().FirstOrDefault(x => x.ProductId == product.ProductId);
+            var p = Products.FirstOrDefault(x => x.ProductId == product.ProductId);
 
             if (p == null)
             {
